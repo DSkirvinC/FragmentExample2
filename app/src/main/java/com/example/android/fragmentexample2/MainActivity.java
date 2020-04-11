9bf33ec10b2e2d9d037f8095ca10d0e673ad256c
DSkirvinC <dskirvinc@gmail.com>
2020-04-10 20:21:17 -0400

    Committed Changes as listed in Homework of Activity Advanced Android 01.1: Fragments
    Signed-off-by: DSkirvinC <dskirvinc@gmail.com>

--- app/src/main/java/com/example/android/fragmentexample2/MainActivity.java
+++ app/src/main/java/com/example/android/fragmentexample2/MainActivity.java
@@ -16,6 +16,7 @@
 
 package com.example.android.fragmentexample2;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -92,5 +93,9 @@ public class MainActivity extends AppCompatActivity {
     public void onSaveInstanceState(Bundle savedInstanceState) {
         savedInstanceState.putBoolean(STATE_FRAGMENT, isFragmentDisplayed);
         super.onSaveInstanceState(savedInstanceState);
+    }
+    public void onNextClick(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
